cfb1cc3c58369ab38f6796951ef4c2be3834a411
RAMYA-V-7 <98602056+RAMYA-V-7@users.noreply.github.com>
2022-05-08 16:42:48 +0530

    Add files via upload

---
+++ main/java/BookFolder/Ex_3_Book.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package BookFolder;
+
+/**
+ *
+ * @author 91984
+ */
+public class Ex_3_Book {
+    String author;
+    String title;
+    double price;
+    String publisher;
+    int stock;
+public Ex_3_Book(String author, String title, double price, String publisher, int stock) {
+        this.author = author;
+        this.title = title;
+        this.price = price;
+        this.publisher = publisher;
+        this.stock = stock;
+    }
+}  
+
---
+++ main/java/WeatherFolder/Ex_2_Weather.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package WeatherFolder;
+
+/**
+ *
+ * @author 91984
+ */
+public class Ex_2_Weather {
+    String city;
+    String maxTemp;
+    String minTemp;
+    String status;
+ public Ex_2_Weather(String city, String maxTemp, String minTemp, String status) {
+        this.city = city;
+        this.maxTemp = maxTemp;
+        this.minTemp = minTemp;
+        this.status = status;
+    }
+}
